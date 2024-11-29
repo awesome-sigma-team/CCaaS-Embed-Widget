648691b9b79ce1dc18af8c1aa84f0d30d29ca4aa
Vertika Pandey <vepandey@microsoft.com>
2024-11-29 16:33:44 +0530

    Added gitignore and updated dynamicsurl as query param

--- ccaas-website-integration/src/IFrameComponent.js
+++ ccaas-website-integration/src/IFrameComponent.js
@@ -1,11 +1,41 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import './App.css';
-export const IFrameComponent = ({setSoftPhonePanelVisibility, softPhonePanelVisibilty}) => (
-    <div id="inqC2CContainer" style={{position: "fixed", bottom: "0px", left: "0px", display:'flex', flexDirection:'column'}}>
-      <iframe 
-      style={{visibility: softPhonePanelVisibilty? 'visible': 'hidden', resize: 'horizontal'}} 
-      src={`https://ccaas-embed-test.azureedge.net/ccaaswidget/index.html?dynamicsUrl=https://msdynccaasdemo.crm.dynamics.com/&&ctiDriverUrl=https://ccaas-widget-integration-demo.azurewebsites.net/js/openCTI.js`} 
-      height="700" 
-      width="595px"></iframe>
-      <button onClick={() => setSoftPhonePanelVisibility(!softPhonePanelVisibilty)}>Live Chat</button>
-    </div>)
+
+export const IFrameComponent = ({
+  setSoftPhonePanelVisibility,
+  softPhonePanelVisibilty,
+}) => {
+  // Extract 'dynamicsUrl' from the query parameters
+  const params = new URLSearchParams(window.location.search);
+  const dynamicsUrl = params.get('dynamicsUrl'); // Get the dynamicsUrl parameter
+  const iframeSrc = dynamicsUrl
+    ? `https://ccaas-embed-prod.azureedge.net/widget/index.html?dynamicsUrl=${encodeURIComponent(
+        dynamicsUrl
+      )}`
+    : ''; // Provide a fallback if dynamicsUrl is not present
+  return (
+    <div
+      id="inqC2CContainer"
+      style={{
+        position: 'fixed',
+        bottom: '0px',
+        left: '0px',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <iframe
+        style={{
+          visibility: softPhonePanelVisibilty ? 'visible' : 'hidden',
+          resize: 'horizontal',
+        }}
+        src={iframeSrc}
+        height="500"
+        width="795px"
+      ></iframe>
+      <button onClick={() => setSoftPhonePanelVisibility(!softPhonePanelVisibilty)}>
+        Live Chat
+      </button>
+    </div>
+  );
+};
